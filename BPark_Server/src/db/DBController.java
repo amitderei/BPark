4204b748f83391a5eac0585121981aa6c8fbe98b
RavidShalev <ravidxcx@gmail.com>
2025-06-10 15:42:31 +0300

    Update DBController.java

--- BPark_Server/src/db/DBController.java
+++ BPark_Server/src/db/DBController.java
@@ -60,7 +60,7 @@ public class DBController {
 		try {
 			// Connects to the local MySQL server (replace credentials as needed)
 			conn = DriverManager.getConnection("jdbc:mysql://localhost/bpark?serverTimezone=Asia/Jerusalem", "root",
-					"Yosi2311");
+					"Aa123456");
 			System.out.println("SQL connection succeed");
 		} catch (SQLException ex) {
 			// Prints SQL error information if connection fails
